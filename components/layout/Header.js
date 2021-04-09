8cf792bf0ec58e28f56e292b083923a19687aef9
Manuel <mninahuanca4@gmail.com>
2021-04-09 00:47:25 -0400

    Agregando elementos al Header

---
+++ components/layout/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Buscar from '../ui/Buscar'
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <div>
+                    <p>p</p>
+                    <Buscar />
+                    {/* { buscador aqui } */}
+                    {/* { nav aqui } */}
+                </div>
+
+                <div>
+                    {/* { menu de adminitracion } */}
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
