21b7f83cfa1e34edad54f518fa4c048fdb117479
Manuel <mninahuanca4@gmail.com>
2021-04-09 01:18:32 -0400

    Agregando fuentes de google fonts y mas

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import Header from './Header';
 import { Global, css } from '@emotion/react';
+import Head from 'next/head';
 
 const Layout = props => {
     return (
@@ -27,11 +28,23 @@ const Layout = props => {
                     body {
                         font-size: 1.6rem;
                         line-height:1.5;
+                        font-family: 'Ubuntu', sans-serif;
+                        font-weight: 300;
                     }
 
                     h1,h2,h3 {
                         margin: 0 0 2rem 0;
                         line-height: 1.5
+                    }
+
+                    h1,h2 {
+                        font-family: 'Ubuntu', sans-serif;
+                        font-weight: 700;
+                    }
+
+                    h3{
+                        font-family: 'Ubuntu', sans-serif;
+                        font-weight: 500
                     }
 
                     ul {
@@ -44,6 +57,14 @@ const Layout = props => {
                     }
                 `}
             />
+            <Head>
+                <html lang="es" />
+                <title>Product Hunt Firebase y Next.js</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;500;700&display=swap" rel="stylesheet" />
+                <link href="/static/css/app.css" rel="stylesheet" />
+            </Head>
 
             <Header />
             <main>
