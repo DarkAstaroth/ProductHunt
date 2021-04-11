d1d25ad43d3712cc8d9419a6d5495deb041f0079
Manuel <mninahuanca4@gmail.com>
2021-04-11 09:52:24 -0400

    Creando el context de firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import App from 'next/app';
+import firebase, { FirebaseContext } from '../firebase';
 
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  
+  return (
+    <FirebaseContext.Provider
+      value={{
+        firebase
+      }}
+    >
+      <Component {...pageProps} />
+    </FirebaseContext.Provider>
+  );
 }
 
 export default MyApp
