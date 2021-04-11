87231141355cd9c7d8ec7e02e24e1f2e1a7af3fe
Manuel <mninahuanca4@gmail.com>
2021-04-10 23:16:58 -0400

    Creando el formulario para crear cuentas

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,3 +1,4 @@
+import React, { Fragment } from 'react';
 import styled from '@emotion/styled';
 import Layout from '../components/layout/Layout';
 
@@ -5,7 +6,49 @@ export default function CrearCuenta() {
   return (
     <div>
       <Layout>
-        <h1>Crear Cuenta</h1>
+        <Fragment>
+          <h1>Crear Cuenta</h1>
+          <form>
+            <div>
+              <label htmlFor="nombre">Nombre:</label>
+              <input
+                type="text"
+                name="nombre"
+                id="nombre"
+                placeholder="Tu Nombre"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="Tu Email"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Tu Password"
+              />
+            </div>
+
+            <div>
+              <input
+                type="submit"
+                value="Crear cuenta"
+              />
+            </div>
+
+            
+          </form>
+        </Fragment>
       </Layout>
 
     </div>
