03e23331773627217997c707cca4536c022c5c70
Manuel <mninahuanca4@gmail.com>
2021-04-13 17:21:03 -0400

    Creando el formulario para crear productos

--- pages/nuevo-producto.js
+++ pages/nuevo-producto.js
@@ -1,11 +1,149 @@
+import React, { Fragment, useState } from 'react';
+import { css } from '@emotion/react';
+import Router from 'next/router';
 import styled from '@emotion/styled';
 import Layout from '../components/layout/Layout';
+import { Formulario, Campo, InputSubmit, Error } from '../components/ui/Formulario';
+
+import firebase from '../firebase';
+
+// Validaciones
+import useValidacion from '../hooks/useValidacion';
+import validarCrearCuenta from '../validacion/validarCrearCuenta';
+
+const STATE_INICIAL = {
+  nombre: '',
+  empresa: '',
+  imagen: '',
+  url: '',
+  descripcion:''  
+}
+const firebaseErrors = {
+  'auth/email-already-in-use': 'La dirección de correo electrónico ya está siendo utilizada por otra cuenta.'
+}
+
 
 export default function NuevoProducto() {
+
+  const [error, setError] = useState(false);
+
+  const {
+    valores,
+    errores,
+    submitFrom,
+    handleChange,
+    handleSubmit,
+    handleBlur
+  } = useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta);
+
+  const { nombre, empresa, imagen, url, descripcion } = valores;
+
+  async function crearCuenta() {
+
+  }
+
   return (
     <div>
       <Layout>
-        <h1>Nuevo Producto</h1>
+        <Fragment>
+          <h1
+            css={css`
+              text-align:center;
+              margin-top:5rem;
+            `}
+          >Nuevo Producto</h1>
+          <Formulario
+            onSubmit={handleSubmit}
+            noValidate
+          >
+
+            <fieldset>
+              <legend>Informacion General</legend>
+              <Campo>
+                <label htmlFor="nombre">Nombre:</label>
+                <input
+                  type="text"
+                  name="nombre"
+                  id="nombre"
+                  placeholder="Tu Nombre"
+                  value={nombre}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+              {errores.nombre && <Error>{errores.nombre}</Error>}
+
+              <Campo>
+                <label htmlFor="empresa">Empresa:</label>
+                <input
+                  type="text"
+                  name="empresa"
+                  id="empresa"
+                  placeholder="Nombre de Empresa o Compañia"
+                  value={empresa}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+              {errores.empresa && <Error>{errores.empresa}</Error>}
+
+              <Campo>
+                <label htmlFor="imagen">Imagen:</label>
+                <input
+                  type="file"
+                  name="imagen"
+                  id="imagen"
+                  value={imagen}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+              {errores.imagen && <Error>{errores.imagen}</Error>}
+
+              <Campo>
+                <label htmlFor="url">Url:</label>
+                <input
+                  type="text"
+                  name="url"
+                  id="url"
+                  // placeholder="Nombre de url o Compañia"
+                  value={url}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+              {errores.url && <Error>{errores.url}</Error>}
+            </fieldset>
+
+            <fieldset>
+              <legend>Sobre tu producto</legend>
+              <Campo>
+                <label htmlFor="descripcion">Descripcion:</label>
+                <textarea
+                  name="descripcion"
+                  id="descripcion"
+                  value={descripcion}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+              {errores.descripcion && <Error>{errores.descripcion}</Error>}
+            </fieldset>
+
+
+
+
+            {error ? <Error>{error}</Error> : null}
+            <Campo>
+              <InputSubmit
+                type="submit"
+                value="Crear Producto"
+              />
+            </Campo>
+
+
+          </Formulario>
+        </Fragment>
       </Layout>
 
     </div>
