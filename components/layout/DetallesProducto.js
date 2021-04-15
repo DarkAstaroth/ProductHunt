7c1fc8e8b49b59ee224f3409d4ce57fe20ab926c
Manuel <mninahuanca4@gmail.com>
2021-04-15 16:20:42 -0400

    Mostrando informacion de la base de datos

--- components/layout/DetallesProducto.js
+++ components/layout/DetallesProducto.js
@@ -1,19 +1,37 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import formarDistanceToNow from 'date-fns/formatDistanceToNow';
+import { es } from 'date-fns/locale';
 
-const DetallesProducto = ({producto}) => {
+const Imagen = styled.img`
+    width : 200px;
+`;
+
+const DetallesProducto = ({ producto }) => {
     console.log(producto);
-    const {id,comentarios,creado,descripcion,empresa,nombre,url,urlimagen,votos} = producto
+    const { id, comentarios, creado, descripcion, empresa, nombre, url, urlImagen, votos } = producto
     return (
         <li>
             <div>
-                <div></div>
                 <div>
-                    <h1>{ nombre }</h1>
+                    <Imagen src={urlImagen} alt="" />
+                </div>
+                <div>
+                    <h1>{nombre}</h1>
+                    <p>{descripcion}</p>
+                    <div>
+                        <img src={"/static/img/comentario.png"} alt="" />
+                        <p>{comentarios.length} Comentarios</p>
+                    </div>
+                    <p>Publicado hace : {formarDistanceToNow(new Date(creado), { locale: es })}</p>
                 </div>
             </div>
-            <div></div>
+            <div>
+                <div>&#9650;</div>
+                <p>{votos}</p>
+            </div>
         </li>
-     );
+    );
 }
- 
+
 export default DetallesProducto;
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -64,6 +64,7 @@ const Layout = props => {
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=PT+Sans&family=Ubuntu:wght@300;500;700&display=swap" rel="stylesheet" />
+
             </Head>
 
             <Header />
