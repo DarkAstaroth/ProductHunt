daee3c43c154e7b9dac92da09f4ffa51792f872a
Manuel <mninahuanca4@gmail.com>
2021-04-16 10:45:07 -0400

    primeros pasos con el Routing dinamico

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -1,8 +1,20 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 const Producto = () => {
+
+    // Routing para obtener el id actual
+    const router = useRouter();
+    const { query: { id } } = router;
+
+    useEffect(() => {
+        if (id) {
+            
+        }
+    }, [id])
+
     return (
-        <h1>desde [id].js</h1>
+        <h1>desde [id]</h1>
      );
 }
  
